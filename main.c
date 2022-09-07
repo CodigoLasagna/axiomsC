2dbc4d30956390dedcdb8af2366346fd48b923a4
hanckeddison <hanckeddison@gmail.com>
2022-09-07 12:36:59 -0700

    base space composer

--- main.c
+++ main.c
@@ -2,35 +2,60 @@
 #include <stdlib.h>
 #include "bools.h"
 
+/*show P and Q*/
+/*erases spaces from string*/
+void cleanAxiom(char*);
+/*divide axiom*/
+void disolveAxiom(char*);
+/*erases screen*/
 void clean();
-void cleanAxiom(char(*)[]);
-int strlength(char* );
+/*returns string size*/
+int strlength(char*);
 
 int main(){
 	char* axiom = malloc(16);
 	char p, q;
 	bool P[] = {true, true, false, false}, Q[] = {true, false, true, false};
+	printf("introduzca P:\n");
+	scanf(" %c", &p);
+	printf("introduzca Q:\n");
+	scanf(" %c", &q);
 	scanf(" %[^\n]", axiom);
+	cleanAxiom(axiom);
+	printf("%s", axiom);
 	return 0;
 }
 
-void cleanAxiom(char(*axiom)[]){
-	int i;
-	for (i = 0; (*axiom)[i] != '\n'; ++i){
-
+void cleanAxiom(char* axiom){
+	short i;
+	int count = 0;
+	for (i = 0; axiom[i] != '\0'; ++i){
+		if (axiom[i] != ' '){
+			axiom[count] = axiom[i];
+			count++;
+		}
 	}
-
+	axiom[count] = '\0';
 }
+
+void disolveAxiom(char* axiom){
+	short i;
+	for (i = 0; axiom[i] != '\0'; ++i){
+		
+	}
+}
+
 void clean(){
-	int i;
+	short i;
 	printf("\033[0;0H");
 	for (i = 0; i < 35; i++)
 		printf("                                     \n");
 	printf("\033[0;0H");
 }
-int strlength(char* axiom){
-	int counter = 0, i;
-	for (i = 0; axiom[i] != '\0'; ++i){
+
+int strlength(char* str){
+	short counter = 0, i;
+	for (i = 0; str[i] != '\0'; ++i){
 		counter++;
 	}
 	return counter;
