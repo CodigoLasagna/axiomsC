afcd5bacf7ed721d8bf8fab6f5fd53739f933ff6
hanckeddison <hanckeddison@gmail.com>
2022-09-13 20:12:50 -0700

    works with three literals

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ void cond(bool[], bool[], bool*);
 void bic(bool[], bool[], bool*);
 
 /*solver for a side*/
-void solveSimpleAxiom(char*, char, char, bool[], bool*);
+void solveSimpleAxiom(char*, char, char, char, bool[], bool*);
 /*decompose complex axiom*/
 void solveCompoundAxiom(char*, char*, char*, bool*);
 /*check variable for var list*/
-void printVar(char, char, char, bool [], bool []);
+void printVar(char, bool []);
 /*print values from var*/
 void printValue(char*, bool []);
 /*erases spaces from string*/
@@ -24,24 +24,28 @@ void cleanAxiom(char*);
 int main(){
 	char* compoundAxiom = malloc(16), *sideA = malloc(16), *sideB = malloc(16);
 	bool parts = false, debug = true;
-	char v, P, Q;
+	char v, P, Q, R;
 	bool tTable[] = {true, true, false, false,
-					 true, false, true, false};
+					 true, false, true, false,
+					 true, false, false, true};
 	bool ansA[4], ansB[4];
 	int i;
-	
 	printf("introduzca P:\n");
 	scanf(" %c", &P);
 	printf("introduzca Q:\n");
 	scanf(" %c", &Q);
+	printf("introduzca R:\n");
+	scanf(" %c", &R);
 	printf("Sus literales son\n");
-	printVar(P, P, Q, tTable, tTable+4);
-	printVar(Q, P, Q, tTable, tTable+4);
+	printVar(P, tTable);
+	printVar(Q, tTable+4);
+	printVar(R, tTable+8);
 	if (debug == true){
 		printf("introduzca una literal simple como '%c'\nuna proposicion simple como la de abajo\n", P);
 		printf("%c>%c\no un axioma complejo como alguno de los siguientes\n", P, Q);
 		printf("%c>%c=!%c>!%c\n", P, Q, Q, P);
 		printf("%c-%c=(%c>%c)^(%c<%c)\n", P, Q, P, Q, P, Q);
+		printf("%cv(%c^%c)=(%cv%c)^(%cv%c)\n", P, Q, R, P, Q, P, R);
 		printf("las siguientes conectivas logicas pueden ser usadas\n");
 		printf("[!]negacion\n[^]conjucion\n[v]disyuncion\n[>]condicional izq. a der.\n[<]condicional der. a izq.\n[-]bicondicional\n");
 	}
@@ -53,11 +57,11 @@ int main(){
 		cleanAxiom(compoundAxiom);
 		solveCompoundAxiom(compoundAxiom, sideA, sideB, &parts);
 		if (parts == false){
-			solveSimpleAxiom(sideA, P, Q, tTable, ansA);
+			solveSimpleAxiom(sideA, P, Q, R, tTable, ansA);
 			printValue(sideA, ansA);
 		}else{
-			solveSimpleAxiom(sideA, P, Q, tTable, ansA);
-			solveSimpleAxiom(sideB, P, Q, tTable, ansB);
+			solveSimpleAxiom(sideA, P, Q, R, tTable, ansA);
+			solveSimpleAxiom(sideB, P, Q, R, tTable, ansB);
 			printValue(sideA, ansA);
 			printValue(sideB, ansB);
 			checkEquality(ansA, ansB);
@@ -80,7 +84,7 @@ void checkEquality(bool sideA[], bool sideB[]){
 	}
 }
 
-void solveSimpleAxiom(char* simpleAxiom, char P, char Q, bool tTable[], bool ans[]){
+void solveSimpleAxiom(char* simpleAxiom, char P, char Q, char R, bool tTable[], bool ans[]){
 	unsigned int i, j;
 	int selector = 0, childSelector = 0, literal = 1;
 	char* child = malloc(16), connector = '0', v = ' ';
@@ -102,12 +106,15 @@ void solveSimpleAxiom(char* simpleAxiom, char P, char Q, bool tTable[], bool ans
 			negative = false;
 		}
 		if (recursive == false){
-			if (simpleAxiom[i] == P || simpleAxiom[i] == Q){
+			if (simpleAxiom[i] == P || simpleAxiom[i] == Q || simpleAxiom[i] == R){
 				if (simpleAxiom[i] == P){
 					literal = 0;
 				}
 				if (simpleAxiom[i] == Q){
 					literal = 1;
+				}
+				if (simpleAxiom[i] == R){
+					literal = 2;
 				}
 				for (j = 0; j < 4; j++){
 					solution[selector][j] = negative ? !tTable[literal*4+j]:tTable[literal*4+j];
@@ -124,7 +131,7 @@ void solveSimpleAxiom(char* simpleAxiom, char P, char Q, bool tTable[], bool ans
 			child[childSelector] = simpleAxiom[i];
 			childSelector++;
 			child[childSelector+1] = '\0';
-			solveSimpleAxiom(child, P, Q, tTable, childAns);
+			solveSimpleAxiom(child, P, Q, R, tTable, childAns);
 			for (j = 0; j < 4; j++){
 				solution[selector][j] = negative ? !childAns[j]:childAns[j];
 			}
@@ -222,13 +229,9 @@ void bic(bool P[], bool Q[], bool PC[]){
 	}
 }
 
-void printVar(char v, char P, char Q, bool PA[], bool QA[]){
-	if (v == P){
-		printValue(&v, PA);
-	}
-	if (v == Q){
-		printValue(&v, QA);
-	}
+void printVar(char v, bool QA[]){
+	char* t = &v;
+	printValue(t, QA);
 }
 
 void printValue(char* v, bool VA[]){
